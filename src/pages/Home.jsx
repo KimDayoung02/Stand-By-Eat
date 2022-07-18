1a64298d18d568245b5131ce366847230a86e315
KimDayoung <dayoung7833@gmail.com>
2022-07-18 10:29:17 +0900

    Feat : Home 레이아웃 작성
    Home의 레이아웃을 작성완료했습니다.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,91 @@
+import { Carousel } from "react-bootstrap";
+import styled from "styled-components";
+function Home() {
+  return (
+    <>
+    <HomeDiv>
+      <Carousel style={{ margin: "4rem 0 4rem 0" }} fade>
+        <Carousel.Item interval={1500}>
+          <CarouselItemImg
+            className="d-block w-100"
+            src="food1.jpeg"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>첫번째 맛집</h3>
+            <p>맛있는 고깃집</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+
+        <Carousel.Item interval={1500}>
+          <CarouselItemImg
+            className="d-block w-100"
+            src="food2.jpg"
+            alt="Second slide"
+          />
+
+          <Carousel.Caption>
+            <h3>두번째 맛집</h3>
+            <p>맛있는 한정식집</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+
+        <Carousel.Item interval={1500}>
+          <CarouselItemImg
+            className="d-block w-100"
+            src="food3.jpg"
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>세번째 맛집</h3>
+            <p>맛있는 한우고기집</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </HomeDiv>
+
+    <ReservationButton>
+        <Button>
+            전체
+        </Button>
+        <Button>
+            부산
+        </Button>
+        <Button>
+            울산
+        </Button>
+        <Button>
+            대구
+        </Button>
+
+    </ReservationButton>
+    </>
+  );
+}
+
+const HomeDiv = styled.div`
+  width: 80%;
+  height: 80%;
+  overflow: hidden;
+  margin: 0 auto;
+  text-align: center;
+`;
+const CarouselItemImg = styled.img`
+  width: 100%;
+  height: 25rem;
+  object-fit: cover;
+`;
+const ReservationButton=styled.div`
+    display: flex;
+    justify-content: space-evenly;
+`
+const Button=styled.button`
+    width:8rem;
+    height:5rem;
+    border-radius: 20px;
+    border-color: white;
+    background-color: beige;
+    text-decoration-style: none;
+`
+export default Home;
