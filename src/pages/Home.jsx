77b829d8bc38ba611fd69815949f57bde5399464
KimDayoung <dayoung7833@gmail.com>
2022-07-20 14:47:26 +0900

    Feat : home 버튼의 수정 및 prettier 적용
    프리티어를 적용하였으며 home의 Button을 수정했습니다.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,65 +1,56 @@
-import { Carousel } from "react-bootstrap";
-import styled from "styled-components";
+import { Carousel, Button } from 'react-bootstrap';
+import styled from 'styled-components';
 function Home() {
   return (
     <>
-    <HomeDiv>
-      <Carousel style={{ margin: "4rem 0 4rem 0" }} fade>
-        <Carousel.Item interval={1500}>
-          <CarouselItemImg
-            className="d-block w-100"
-            src="food1.jpeg"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>첫번째 맛집</h3>
-            <p>맛있는 고깃집</p>
-          </Carousel.Caption>
-        </Carousel.Item>
+      <HomeDiv>
+        <Carousel style={{ margin: '4rem 0 4rem 0' }} fade>
+          <Carousel.Item interval={1500}>
+            <CarouselItemImg
+              className="d-block w-100"
+              src="food1.jpeg"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>첫번째 맛집</h3>
+              <p>맛있는 고깃집</p>
+            </Carousel.Caption>
+          </Carousel.Item>
 
-        <Carousel.Item interval={1500}>
-          <CarouselItemImg
-            className="d-block w-100"
-            src="food2.jpg"
-            alt="Second slide"
-          />
+          <Carousel.Item interval={1500}>
+            <CarouselItemImg
+              className="d-block w-100"
+              src="food2.jpg"
+              alt="Second slide"
+            />
 
-          <Carousel.Caption>
-            <h3>두번째 맛집</h3>
-            <p>맛있는 한정식집</p>
-          </Carousel.Caption>
-        </Carousel.Item>
+            <Carousel.Caption>
+              <h3>두번째 맛집</h3>
+              <p>맛있는 한정식집</p>
+            </Carousel.Caption>
+          </Carousel.Item>
 
-        <Carousel.Item interval={1500}>
-          <CarouselItemImg
-            className="d-block w-100"
-            src="food3.jpg"
-            alt="Third slide"
-          />
+          <Carousel.Item interval={1500}>
+            <CarouselItemImg
+              className="d-block w-100"
+              src="food3.jpg"
+              alt="Third slide"
+            />
 
-          <Carousel.Caption>
-            <h3>세번째 맛집</h3>
-            <p>맛있는 한우고기집</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-    </HomeDiv>
+            <Carousel.Caption>
+              <h3>세번째 맛집</h3>
+              <p>맛있는 한우고기집</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </HomeDiv>
 
-    <ReservationButton>
-        <Button>
-            전체
-        </Button>
-        <Button>
-            부산
-        </Button>
-        <Button>
-            울산
-        </Button>
-        <Button>
-            대구
-        </Button>
-
-    </ReservationButton>
+      <ReservationButton>
+        <Buttons>전체</Buttons>
+        <Buttons>부산</Buttons>
+        <Buttons>울산</Buttons>
+        <Buttons>대구</Buttons>
+      </ReservationButton>
     </>
   );
 }
@@ -76,16 +67,24 @@ const CarouselItemImg = styled.img`
   height: 25rem;
   object-fit: cover;
 `;
-const ReservationButton=styled.div`
-    display: flex;
-    justify-content: space-evenly;
-`
-const Button=styled.button`
-    width:8rem;
-    height:5rem;
-    border-radius: 20px;
+const ReservationButton = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
+const Buttons = styled(Button)`
+  width: 8rem;
+  height: 5rem;
+  border-radius: 20px;
+  border-color: #ffffff;
+  color: #6a2490;
+  background-color: #eaccfd;
+  box-shadow: none;
+  &:hover {
+    background-color: #ba86d5;
     border-color: white;
-    background-color: beige;
-    text-decoration-style: none;
-`
+  }
+  &:visited {
+    border-color: #6a2490;
+  }
+`;
 export default Home;
