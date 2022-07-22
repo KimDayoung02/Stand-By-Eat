e957b7781876d2a6155940fab31fa76a3c9d40b6
KimDayoung <dayoung7833@gmail.com>
2022-07-22 15:33:31 +0900

    Design : 가게 리스트 레이아웃
    예약 페이지의 가게 리스트 레이아웃 진행중입니다.

--- src/components/StoreList.jsx
+++ src/components/StoreList.jsx
@@ -2,23 +2,61 @@ import styled from 'styled-components';
 
 function StoreList() {
   return (
-    <TestTag>
-      <DivTag>나는야 퉁퉁이 골목대장이라네</DivTag>
-    </TestTag>
+    <StoreDiv>
+      <StoreContentsDiv>
+        <ImageDiv>
+          <StoreImg src="food1.jpeg" />
+        </ImageDiv>
+        <StoreStringsDiv>
+          <StoreName>가게이름</StoreName>
+          <StoreDetail>가게소개</StoreDetail>
+          <StoreNumber>000-000-0000</StoreNumber>
+        </StoreStringsDiv>
+      </StoreContentsDiv>
+    </StoreDiv>
   );
 }
 
-const DivTag = styled.div`
-  margin-top: 2rem;
-  padding: 1.5rem 3rem;
-  height: auto;
-  width: 90%;
-  background-color: #f8e7f4;
-  border-radius: 30px;
-`;
-const TestTag = styled.div`
+const StoreDiv = styled.div`
   display: flex;
   justify-content: center;
 `;
+
+const StoreContentsDiv = styled.div`
+  margin-top: 2rem;
+  padding: 1.5rem 3rem;
+  height: auto;
+  width: 80%;
+  border: 10px solid #ddb8e8;
+  border-radius: 30px;
+  display: flex;
+`;
+
+const ImageDiv = styled.div`
+  width: 15rem;
+  height: 13rem;
+  position: relative;
+  overflow: hidden;
+`;
+
+const StoreImg = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+const StoreStringsDiv = styled.div`
+  padding: 1rem 2rem;
+`;
+
+const StoreName = styled.div`
+  margin-bottom: 2rem;
+`;
+
+const StoreDetail = styled.div`
+  margin-bottom: 2rem;
+`;
+
+const StoreNumber = styled.div``;
 
 export default StoreList;
