573f21d43645fa09c7a71859ec630264ec0c9678
theo <august4hb@gmail.com>
2022-07-22 17:08:19 +0900

    Signup삭제

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-<<<<<<< HEAD
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import styled from 'styled-components';
-import UserMyPage from './pages/users/myPage';
-import Home from './pages/Home';
-<<<<<<< HEAD
-=======
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import styled from "styled-components";
 import UserMyPage from "./pages/users/myPage";
+import Home from './pages/Home';
 import Signup from "./pages/users/Signup";
 import SignupComplete from "./pages/users/SignupComplete";
->>>>>>> FE/hb
-=======
-import Reservation from './pages/Reservation';
->>>>>>> e957b7781876d2a6155940fab31fa76a3c9d40b6
+
 
 function App() {
   return (
@@ -30,12 +19,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="myPage" element={<UserMyPage />} />
-<<<<<<< HEAD
           <Route path="Signup" element={<Signup />} />
           <Route path="SignupComplete" element={<SignupComplete />} />
-=======
-          <Route path="reservation" element={<Reservation />}></Route>
->>>>>>> e957b7781876d2a6155940fab31fa76a3c9d40b6
         </Routes>
       </BrowserRouter>
     </div>
