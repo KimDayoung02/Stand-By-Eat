3c054b7cfdd89adc749541eb1a822277a3a0a448
[SW2/부산] 정민희 <dev.alsgml1640@gmail.com>
2022-07-25 10:21:28 +0900

    feat : 마이페이지 프로필 관련

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import styled from 'styled-components';
-import UserMyPage from './pages/users/myPage';
+import UserMyPage from './pages/myPage';
 import Home from './pages/Home';
 import Signup from './pages/users/Signup';
-import SignupComplete from './pages/users/SignupComplete';
+//import SignupComplete from './pages/users/SignupComplete';
 //import Signup from './pages/Signup';
 import Login from './pages/TestLogin';
 
@@ -21,7 +21,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="myPage" element={<UserMyPage />} />
           <Route path="Signup" element={<Signup />} />
-          <Route path="SignupComplete" element={<SignupComplete />} />
+          {/* <Route path="SignupComplete" element={<SignupComplete />} /> */}
           <Route path="login" element={<Login />} />
         </Routes>
       </BrowserRouter>
